d7a534b7c71fdb8c84b39face4a553e060b8661c
lukaasos <lucasrangelcoringa@gmail.com>
2024-08-07 13:56:59 -0300

    arquivo gasolina.py criado

---
+++ gasolina.py
@@ -0,0 +1,9 @@
+# código de geração do gráfico 
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+df = pd.read_csv('gasolina.csv')
+
+fig = sns.lineplot(data=df, x=df['dia'], y=df['venda'])
+fig.set(xlabel ="Dia X", ylabel = "Venda", title ='Gasolina')
+#plt.savefig('gasolina.png')
